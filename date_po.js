8bbfe9610341d048f194d5c004bee0b8635c06bb
kelvin.luck <kelvin.luck@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-09-06 21:25:06 +0000

    Added Polish localisation for date methods (provided by eRIZ)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk/plugins/methods@3126 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ date_po.js
@@ -0,0 +1,11 @@
+// date localization for locale 'po'
+// provided by eRIZ
+
+Date.dayNames = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
+Date.abbrDayNames = ['Ni', 'Po', 'Wt', 'Śr', 'Cz', 'Pt', 'So'];
+Date.monthNames = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
+Date.abbrMonthNames = ['Sty', 'Lut', 'Mar', 'Kwi', 'Maj', 'Cze', 'Lip', 'Sie', 'Wrz', 'Paź', 'Lis', 'Gru'];
+
+
+Date.firstDayOfWeek = 1;
+Date.format = 'dd.mmm.yyyy';
