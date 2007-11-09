ee3285c9ddae0c7c499f4a25bfbb6b55c127ea7b
kelvin.luck <kelvin.luck@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-09 15:35:22 +0000

    Added Russian date methods localisation files (provided by Sergey Nechaev)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk/plugins/methods@3804 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ date_ru_win1251.js
@@ -0,0 +1,10 @@
+// date localization for locale 'ru-RU', win-1251 encoding
+// provided by Sergey Nechaev http://nechaev.org/)
+
+Date.dayNames = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+Date.abbrDayNames = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+Date.monthNames = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+Date.abbrMonthNames = ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'];
+
+Date.firstDayOfWeek = 1;
+Date.format = 'dd.mm.yyyy';
