9298293eae4d7601874a94ed81c3bad62287f53a
kelvin.luck <kelvin.luck@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-12-10 20:44:04 +0000

    Added date localization for locale 'ru-UA' (Ukrainian), utf-8 encoding provided by Frankovskyi Bogdan
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk/plugins/methods@4095 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ date_ua_utf8.js
@@ -0,0 +1,11 @@
+// date localization for locale 'ru-UA' (Ukrainian), utf-8 encoding
+// just put it after "date.js" declaration
+// provided by Frankovskyi Bogdan, bfrankovskyi@gmail.com
+
+Date.dayNames = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'Пятниця', 'Субота'];
+Date.abbrDayNames = ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+Date.monthNames = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
+Date.abbrMonthNames = ['Січ', 'Лют', 'Бер', 'Квіт', 'Трав', 'Чер', 'Лип', 'Сер', 'Вер', 'Жов', 'Лис', 'Груд'];
+
+Date.firstDayOfWeek = 1;
+Date.format = 'dd.mm.yyyy';
