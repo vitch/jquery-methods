f88b2c525426024003994cc228428e1193016e89
kelvinluck <kelvinluck@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-06-19 08:34:43 +0000

    Date methods: Added contributed Belarussian localisation (thanks Vladimir)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk/plugins/methods@5734 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ date_be_utf8.js
@@ -0,0 +1,10 @@
+// date localization for locale 'be' (Belarussian), utf-8 encoding
+// provided by Vladimir Prudnikov http://prudnikov.com/)
+
+Date.dayNames = ['Нядзеля', 'Панядзелак', 'Ауторак', 'Серада', 'Чацьвер', 'Пятница', 'Суббота'];
+Date.abbrDayNames = ['Нд', 'Пн', 'Аў', 'Ср', 'Чц', 'Пт', 'Сб'];
+Date.monthNames = ['Студзень', 'Люты', 'Сакавік', 'Красавік', 'Травень', 'Чэрвень', 'Ліпень', 'Жнівень', 'Верасень', 'Кастрычнік', 'Лістапад', 'Снежань'];
+Date.abbrMonthNames = ['Сту', 'Лют', 'Сак', 'Кра', 'Тра', 'Чэр', 'Ліп', 'Жні', 'Вер', 'Кас', 'Ліс', 'Сне'];
+
+Date.firstDayOfWeek = 1;
+Date.format = 'dd.mm.yyyy';
