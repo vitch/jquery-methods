051c99c82aded21100c1e1c81b77f7e898317f04
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-08-27 20:35:52 +0000

    date methods: added pt-br localization, fixed other files (ö encoding)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk/plugins/methods@5840 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ date_pt-br.js
@@ -0,0 +1,6 @@
+// date localization for locale 'pt-br'
+// portuguese Brasil by André Furlan (tatu pheba)
+Date.dayNames = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
+Date.abbrDayNames = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'];
+Date.monthNames = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+Date.abbrMonthNames = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
