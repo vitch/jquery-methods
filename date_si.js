c3bfaf8485ce837f8584fd276cb7318029acbfcb
Kelvin Luck <git@kelvinluck.com>
2010-10-12 09:57:31 +0100

    Added slovenian localisations (see http://code.google.com/p/jquery-datepicker/issues/detail?id=86 )

---
+++ date_si.js
@@ -0,0 +1,5 @@
+// date localization for locale 'si'
+Date.dayNames = ['Nedelja', 'Ponedeljek', 'Torek', 'Sreda', 'Četrtek', 'Petek', 'Sobota'];
+Date.abbrDayNames = ['Ned', 'Pon', 'Tor', 'Sre', 'Čet', 'Pet', 'Sob'];
+Date.monthNames = ['Januar', 'Februar', 'Marec', 'April', 'Maj', 'Junij', 'Julij', 'Avgust', 'September', 'Oktober', 'November', 'December'];
+Date.abbrMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Avg', 'Sep', 'Okt', 'Nov', 'Dec'];
