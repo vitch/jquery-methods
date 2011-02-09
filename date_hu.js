0021c06ebce6ce3cc2577316a134a0aeb2f95dd3
Kelvin Luck <git@kelvinluck.com>
2011-02-09 12:45:15 +0000

    Added Hungarian localisations - thanks Zoltan Majerszki

---
+++ date_hu.js
@@ -0,0 +1,8 @@
+// date localization for locale 'hu'
+// Thanks to Zoltan Majerszki
+Date.dayNames = ['Vasárnap', 'Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat'];
+Date.abbrDayNames = ['V', 'H', 'K', 'Sze', 'Cs', 'P', 'Szo'];
+Date.monthNames = ['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December'];
+Date.abbrMonthNames = ['Jan', 'Feb', 'Márc', 'Ápr', 'Máj', 'Jún', 'Júl', 'Aug', 'Szept', 'Okt', 'Nov', 'Dec'];
+Date.firstDayOfWeek = 1;
+Date.format = 'yyyy.mm.dd.';
