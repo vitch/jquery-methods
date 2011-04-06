e501e0044158d3cd80fb6ef4af9b8b37879c3534
tetsuo13 <andre@neo-anime.org>
2011-04-06 15:29:13 -0600

    Add Japanese locale

---
+++ date_jp.js
@@ -0,0 +1,7 @@
+// Date localization for locale 'jp'.
+// Provided by Andrei Nicholson <andre@neo-anime.org>.
+Date.dayNames = ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'];
+Date.abbrDayNames = ['日', '月', '火', '水', '木', '金', '土'];
+Date.monthNames = ['1 月', '2 月', '3 月', '4 月', '5 月', '6 月', '7 月', '8 月', '9 月', '10 月', '11 月', '12 月'];
+Date.abbrMonthNames = ['1 月', '2 月', '3 月', '4 月', '5 月', '6 月', '7 月', '8 月', '9 月', '10 月', '11 月', '12 月'];
+Date.format = 'yyyy-mm-dd';
