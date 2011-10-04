a0b35a4e0bfe2893f2db9edc0b58a886aadbc9e6
Kelvin Luck <git@kelvinluck.com>
2011-10-04 13:02:22 +0100

    Changed text encoding to utf-8 for the Czech translation...

--- date_cz.js
+++ date_cz.js
@@ -1,10 +1,10 @@
 // date localization for locale 'cz'
 // provided by yesagency (http://www.yesagency.cz/)
 
-Date.dayNames = ['Nedìle', 'Pondìlí', 'Úterý', 'Støeda', 'Ètvrtek', 'Pátek', 'Sobota'];
-Date.abbrDayNames = ['Ne', 'Po', 'Út', 'St', 'Èt', 'Pá', 'So'];
-Date.monthNames = ['Leden', 'Únor', 'Bøezen', 'Duben', 'Kvìten', 'Èerven', 'Èervenec', 'Srpen', 'Záøí', 'Øíjen', 'Listopad', 'Prosinec'];
-Date.abbrMonthNames = ['Led', 'Úno', 'Bøe', 'Dub', 'Kvì', 'Erven', 'Ervenec', 'Srp', 'Záø', 'Øíj', 'Lis', 'Pro'];
+Date.dayNames = ['NedÃ¬le', 'PondÃ¬lÃ­', 'ÃšterÃ½', 'StÃ¸eda', 'Ãˆtvrtek', 'PÃ¡tek', 'Sobota'];
+Date.abbrDayNames = ['Ne', 'Po', 'Ãšt', 'St', 'Ãˆt', 'PÃ¡', 'So'];
+Date.monthNames = ['Leden', 'Ãšnor', 'BÃ¸ezen', 'Duben', 'KvÃ¬ten', 'Ãˆerven', 'Ãˆervenec', 'Srpen', 'ZÃ¡Ã¸Ã­', 'Ã˜Ã­jen', 'Listopad', 'Prosinec'];
+Date.abbrMonthNames = ['Led', 'Ãšno', 'BÃ¸e', 'Dub', 'KvÃ¬', 'Erven', 'Ervenec', 'Srp', 'ZÃ¡Ã¸', 'Ã˜Ã­j', 'Lis', 'Pro'];
 
 
 Date.firstDayOfWeek = 1;
