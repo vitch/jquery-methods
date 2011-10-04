89e86c188529e1533327f80bf7d48766f1a4e7c3
Kelvin Luck <git@kelvinluck.com>
2011-10-04 13:00:30 +0100

    Added Czech localisation.
    Thanks to tibor.ma...@gmail.com [ http://code.google.com/p/jquery-datepicker/issues/detail?id=316 ]

---
+++ date_cz.js
@@ -0,0 +1,11 @@
+// date localization for locale 'cz'
+// provided by yesagency (http://www.yesagency.cz/)
+
+Date.dayNames = ['Nedìle', 'Pondìlí', 'Úterý', 'Støeda', 'Ètvrtek', 'Pátek', 'Sobota'];
+Date.abbrDayNames = ['Ne', 'Po', 'Út', 'St', 'Èt', 'Pá', 'So'];
+Date.monthNames = ['Leden', 'Únor', 'Bøezen', 'Duben', 'Kvìten', 'Èerven', 'Èervenec', 'Srpen', 'Záøí', 'Øíjen', 'Listopad', 'Prosinec'];
+Date.abbrMonthNames = ['Led', 'Úno', 'Bøe', 'Dub', 'Kvì', 'Erven', 'Ervenec', 'Srp', 'Záø', 'Øíj', 'Lis', 'Pro'];
+
+
+Date.firstDayOfWeek = 1;
+Date.format = 'dd.mmm.yyyy';
