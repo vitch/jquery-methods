0fec5f0cb2d4f90d5406b209ec254d8cb002c3e1
Yiqiu Huang <huangyq23@gmail.com>
2012-07-06 16:10:04 +0800

    Added zh-CN locale.

---
+++ date_zh-cn.js
@@ -0,0 +1,7 @@
+// Date localization for locale 'zh-cn'.
+// Provided by husher (huangyq23@gmail.com).
+Date.dayNames = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+Date.abbrDayNames = ['日', '一', '二', '三', '四', '五', '六'];
+Date.monthNames = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+Date.abbrMonthNames = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'];
+Date.format = 'yyyy/mm/dd';
