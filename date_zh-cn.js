0a068a7215747bd97e17e9fc9f8d287ec6cb061c
Yiqiu Huang <huangyq23@gmail.com>
2012-07-06 16:22:10 +0800

    Change zh-cn locale's First Day Of Week to Sunday.

--- date_zh-cn.js
+++ date_zh-cn.js
@@ -4,4 +4,5 @@ Date.dayNames = ['星期日', '星期一', '星期二', '星期三', '星期四'
 Date.abbrDayNames = ['日', '一', '二', '三', '四', '五', '六'];
 Date.monthNames = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
 Date.abbrMonthNames = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'];
+Date.firstDayOfWeek = 1;
 Date.format = 'yyyy/mm/dd';
